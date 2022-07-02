4a3768c175dd8d1a55a2c9edbe9c5c52624763ee
mtelewa <mohamed.hassan@kit.edu>
2022-07-02 10:34:13 +0200

    added bind to workspace

--- pentane/nemd-vibrating-walls/submit_sim_singularity.sh
+++ pentane/nemd-vibrating-walls/submit_sim_singularity.sh
@@ -14,4 +14,4 @@ export KMP_AFFINITY=compact,1,0
 module load compiler/intel/19.1
 module load mpi/openmpi/4.0
 
-mpirun --bind-to core --map-by core singularity run --bind /scratch --bind /tmp --pwd=$PWD $HOME/programs/lammps.sif -i $(pwd)/flow.LAMMPS
+mpirun --bind-to core --map-by core singularity run --bind /pfs/work7/workspace/scratch/lr1762-flow_sims/ --bind /scratch --bind /tmp --pwd=$PWD $HOME/programs/lammps.sif -i $(pwd)/flow.LAMMPS
