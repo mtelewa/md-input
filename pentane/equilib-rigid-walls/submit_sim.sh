307f3a3e57d060d82da9060c9db001cc11944bf6
mtelewa <mohamed.hassan@kit.edu>
2022-11-12 15:24:28 +0100

    fixed the walltime

--- pentane/equilib-rigid-walls/submit_sim.sh
+++ pentane/equilib-rigid-walls/submit_sim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=20
-#SBATCH --time=12:00:00
+#SBATCH --time=2:00:00
 #SBATCH --partition=multiple
 #SBATCH --output=cluster.out
 #SBATCH --error=cluster.err
@@ -17,4 +17,4 @@ module load mpi/openmpi/4.0
 # Attention:
 # Do NOT add mpirun options -n <number_of_processes> or any other option defining processes or nodes, since MOAB instructs mpirun about number of processes and node hostnames. Moreover, replace <placeholder_for_version> with the wished version of Intel MPI to enable the MPI environment. 
 
-mpirun --bind-to core --map-by core -report-bindings $HOME/lammps-master/src/lmp_mpi -in $(pwd)/equilib.LAMMPS -v press 285
+mpirun --bind-to core --map-by core -report-bindings lmp_mpi -in $(pwd)/equilib.LAMMPS -v press 285
